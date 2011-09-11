79e9f1e5b7ef1de0a61a9ac1e902ce9b3a84195f
xavier deschuyteneer <xavier.deschuyteneer@gmail.com>
2011-09-11 17:20:36 +0200

    changing usage, README completed, example add

---
+++ Test.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+ 
+public class Test extends JFrame {
+ 
+    public class Panneau extends JPanel {
+ 
+        public void paintComponent(Graphics g){
+            g.fillOval(20, 20, 75, 75);
+        }               
+    }
+    public Test(){
+        
+        this.setTitle("Test");
+        this.setSize(100, 150);
+        this.setLocationRelativeTo(null);               
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(new Panneau());
+        this.setVisible(true);
+        }
+    public static void main(String args[])
+    {
+        Test fenetre = new Test();
+    }
+    
+    
+        
+}
